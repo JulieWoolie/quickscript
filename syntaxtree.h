eb98c6337f1500d1f91b8282e022d2314ec0b3ca
JulieWoolie <andero.magi.420@gmail.com>
2026-07-14 21:26:25 +0300

    Add missing binary op types and missing AST nodes

--- syntaxtree.h
+++ syntaxtree.h
@@ -32,6 +32,16 @@ AST_TYPE(Identifier, Expr,
   stringid value = EMPTY_STRING;
 )
 
+AST_TYPE(CallExpr, Expr,
+  NodeRef<Expr> target = UNSET_REF;
+  std::vector<NodeRef<Expr>> arguments;
+)
+
+AST_TYPE(PropertyAccessExpr, Expr,
+  NodeRef<Identifier> property = UNSET_REF;
+  NodeRef<Expr> target = UNSET_REF;
+)
+
 AST_TYPE(BooleanLiteral, Expr,
   bool value = false;
 )
@@ -54,40 +64,45 @@ AST_TYPE(FloatLiteral, Expr,
 
 // --- Binary Operations ---
 
-#define BOP_ASSIGN_FLAG  0b100000
-#define BOP_NIL          0b000000
+#define BOP_ASSIGN_FLAG     0b100000
+#define BOP_NIL             0b000000
 
-#define BOP_GT           0b000001
-#define BOP_LT           0b000010
-#define BOP_GTE          0b000011
-#define BOP_LTE          0b000100
-#define BOP_EQ           0b000101
-#define BOP_NEQ          0b000110
-#define BOP_ADD          0b000111
-#define BOP_SUB          0b001000
-#define BOP_MUL          0b001001
-#define BOP_DIV          0b001010
-#define BOP_MOD          0b001011
-#define BOP_EXP          0b001100
-#define BOP_SHL          0b001101
-#define BOP_SHR          0b001110
-#define BOP_USHR         0b001111
-#define BOP_XOR          0b010000
-#define BOP_OR           0b010001
-#define BOP_AND          0b010010
+#define BOP_GT              0b000001
+#define BOP_LT              0b000010
+#define BOP_GTE             0b000011
+#define BOP_LTE             0b000100
+#define BOP_EQ              0b000101
+#define BOP_NEQ             0b000110
+#define BOP_ADD             0b000111
+#define BOP_SUB             0b001000
+#define BOP_MUL             0b001001
+#define BOP_DIV             0b001010
+#define BOP_MOD             0b001011
+#define BOP_EXP             0b001100
+#define BOP_SHL             0b001101
+#define BOP_SHR             0b001110
+#define BOP_USHR            0b001111
+#define BOP_XOR             0b010000
+#define BOP_LOG_OR          0b010001
+#define BOP_LOG_AND         0b010010
+#define BOP_BIT_OR          0b010011
+#define BOP_BIT_AND         0b010100
+#define BOP_POW             0b010101
 
-#define BOP_ASSIGN_ADD   (BOP_ADD | BOP_ASSIGN_FLAG)
-#define BOP_ASSIGN_SUB   (BOP_SUB | BOP_ASSIGN_FLAG)
-#define BOP_ASSIGN_MUL   (BOP_MUL | BOP_ASSIGN_FLAG)
-#define BOP_ASSIGN_DIV   (BOP_DIV | BOP_ASSIGN_FLAG)
-#define BOP_ASSIGN_MOD   (BOP_MOD | BOP_ASSIGN_FLAG)
-#define BOP_ASSIGN_EXP   (BOP_EXP | BOP_ASSIGN_FLAG)
-#define BOP_ASSIGN_SHL   (BOP_SHL | BOP_ASSIGN_FLAG)
-#define BOP_ASSIGN_SHR   (BOP_SHR | BOP_ASSIGN_FLAG)
-#define BOP_ASSIGN_USHR  (BOP_USHR | BOP_ASSIGN_FLAG)
-#define BOP_ASSIGN_XOR   (BOP_XOR | BOP_ASSIGN_FLAG)
-#define BOP_ASSIGN_OR    (BOP_OR | BOP_ASSIGN_FLAG)
-#define BOP_ASSIGN_AND   (BOP_AND | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_ADD      (BOP_ADD | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_SUB      (BOP_SUB | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_MUL      (BOP_MUL | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_DIV      (BOP_DIV | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_MOD      (BOP_MOD | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_EXP      (BOP_EXP | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_SHL      (BOP_SHL | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_SHR      (BOP_SHR | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_USHR     (BOP_USHR | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_XOR      (BOP_XOR | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_LOG_OR   (BOP_LOG_OR | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_BIT_OR   (BOP_BIT_OR | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_LOG_AND  (BOP_LOG_AND | BOP_ASSIGN_FLAG)
+#define BOP_ASSIGN_BIT_AND  (BOP_BIT_AND | BOP_ASSIGN_FLAG)
 
 typedef uint8 binaryop;
 
@@ -114,6 +129,12 @@ typedef uint8 unaryop;
 AST_TYPE(UnaryExpr, Expr,
   NodeRef<Expr> target = UNSET_REF;
   unaryop op = UOP_NIL;
+)
+
+AST_TYPE(TernaryExpr, Expr,
+  NodeRef<Expr> condition = UNSET_REF;
+  NodeRef<Expr> left = UNSET_REF;
+  NodeRef<Expr> right = UNSET_REF;
 )
 
 // ========================
