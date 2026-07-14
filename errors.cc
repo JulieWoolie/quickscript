5f3a89cecc4c00a07dc0fbed7f9f6abcdf12b428
JulieWoolie <andero.magi.420@gmail.com>
2026-07-14 12:50:50 +0300

    Add error reporter

---
+++ errors.cc
@@ -0,0 +1,120 @@
+
+#include "errors.h"
+
+#include <stdarg.h>
+
+#define CREATE_LOG_METHOD(name, level) void CompilerErrors::name(Location& loc, conststring msg, ...) {va_list l; va_start(l, msg); log(level, &loc, msg, l); va_end(l);}
+#define CREATE_LOG_METHOD_NL(name, level) void CompilerErrors::name(conststring msg, ...) {va_list l; va_start(l, msg); log(level, nullptr, msg, l); va_end(l);}
+
+CompilerErrors::CompilerErrors(std::string *fileContent, conststring fName) {
+  m_fileContent = fileContent;
+  m_fileName = fName;
+}
+
+CREATE_LOG_METHOD(fatal, LOGL_FATAL)
+CREATE_LOG_METHOD_NL(fatal, LOGL_FATAL)
+
+CREATE_LOG_METHOD(error, LOGL_ERROR)
+CREATE_LOG_METHOD_NL(error, LOGL_ERROR)
+
+CREATE_LOG_METHOD(warn, LOGL_WARN)
+CREATE_LOG_METHOD_NL(warn, LOGL_WARN)
+
+CREATE_LOG_METHOD(info, LOGL_INFO)
+CREATE_LOG_METHOD_NL(info, LOGL_INFO)
+
+static uint32 findLineBoundary(std::string* bufstr, int32 pos, int32 direction) {
+  int32 p = pos;
+  int32 len = bufstr->length();
+
+  while (true) {
+    if (p >= len) {
+      return len - 1;
+    }
+    if (p < 0) {
+      return 0;
+    }
+
+    const char ch = bufstr->at(p);
+
+    if (ch == '\n' || ch == '\r') {
+      if (direction == -1) {
+        return p + 1;
+      }
+      return p;
+    }
+
+    p += direction;
+  }
+}
+
+void repeat(const char c, const uint32 times, char* out) {
+  for (uint32 i = 0; i < times; i++) {
+    out[i] = c;
+  }
+  out[times] = '\0';
+}
+
+conststring loglevel_name(loglevel l) {
+  switch (l) {
+    case LOGL_INFO:  return " INFO";
+    case LOGL_WARN:  return " WARN";
+    case LOGL_ERROR: return "ERROR";
+    case LOGL_FATAL: return "FATAL";
+    default: return "?????";
+  }
+}
+
+void CompilerErrors::log(loglevel level, Location *l, conststring msg, va_list list) {
+  char content[1024];
+  int32 result = vsnprintf_s(content, 1024, 1024, msg, list);
+
+  if (result < 1) {
+    if (level == LOGL_FATAL) {
+      throw std::runtime_error(msg);
+    }
+
+    return;
+  }
+
+  if (!l) {
+    printf_s("[%s] %s\n", loglevel_name(level), content);
+    return;
+  }
+
+  const uint32 linestart = findLineBoundary(m_fileContent, l->index, -1);
+  const uint32 lineend = findLineBoundary(m_fileContent, l->index, 1);
+  const uint32 linelen = lineend - linestart;
+
+  std::string line = m_fileContent->substr(linestart, linelen);
+
+  const uint32 lineno = l->line;
+  const uint32 col = l->column;
+
+  std::string linenoStr = std::to_string(lineno);
+  uint32 len = linenoStr.length() + 1;
+
+  char lpad[len];
+  repeat(' ', len - 1, lpad);
+  lpad[len - 1] = '\0';
+
+  uint32 cpadlen = col < 0 ? 0 : col;
+  char cpad[cpadlen + 1];
+  repeat(' ', cpadlen, cpad);
+  cpad[cpadlen] = '\0';
+
+  printf(
+    "[%s] %s\n%s--> %s:%s:%i\n%s |\n%s | %s\n%s |%s^ %s\n%s |\n",
+    loglevel_name(level),
+    content,
+    lpad, m_fileName, linenoStr.c_str(), col,
+    lpad,
+    lpad, line.c_str(),
+    lpad, cpad, content,
+    lpad
+  );
+
+  if (level == LOGL_FATAL) {
+    throw std::runtime_error(content);
+  }
+}
---
+++ errors.h
@@ -0,0 +1,34 @@
+#ifndef GIT_QUICKSCRIPT_ERRORS_H
+#define GIT_QUICKSCRIPT_ERRORS_H
+#include "lexer.h"
+
+typedef uint8 loglevel;
+
+#define LOGL_INFO  0
+#define LOGL_WARN  1
+#define LOGL_ERROR 2
+#define LOGL_FATAL 3
+
+class CompilerErrors {
+  std::string* m_fileContent;
+  conststring m_fileName;
+
+  public:
+    CompilerErrors(std::string* fileContent, conststring fName);
+
+    void fatal(Location& loc, conststring msg, ...);
+    void fatal(conststring msg, ...);
+
+    void error(Location& loc, conststring msg, ...);
+    void error(conststring msg, ...);
+
+    void warn(Location& loc, conststring msg, ...);
+    void warn(conststring msg, ...);
+
+    void info(Location& loc, conststring msg, ...);
+    void info(conststring msg, ...);
+
+    void log(loglevel level, Location* l, conststring msg, va_list list);
+};
+
+#endif //GIT_QUICKSCRIPT_ERRORS_H
