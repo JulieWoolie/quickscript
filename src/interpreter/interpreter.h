65f0bfe4bdbce4eee3e6a6fbe6c0f372e3288bee
JulieWoolie <andero.magi.420@gmail.com>
2026-07-21 14:15:03 +0300

    Update interpreter.h

--- src/interpreter/interpreter.h
+++ src/interpreter/interpreter.h
@@ -1,4 +1,40 @@
 #ifndef QUICKSCRIPT_INTERPRETER_H
 #define QUICKSCRIPT_INTERPRETER_H
 
+#include "nativeinterface.h"
+#include "../common.h"
+
+#define REGISTERS_COUNT 8
+#define MAX_CALL_DEPTH 128
+
+struct CallFrame {
+  conststring name = nullptr;
+  uint32 lineno = 0;
+};
+
+struct InterpreterState {
+  uint64 registers[REGISTERS_COUNT]{};
+
+  CallFrame frames[MAX_CALL_DEPTH]{};
+  uint32 framesDepth = 0;
+
+  uint32 getLine() const {
+    if (framesDepth <= 0) {
+      return 0;
+    }
+    return frames[framesDepth-1].lineno;
+  }
+};
+
+struct BytecodeFunction {
+  conststring name = nullptr;
+  uint8* instructions = nullptr;
+};
+
+struct BytecodeFile {
+  uint8* stringConstPool = nullptr;
+  NativeFunction* nativeFunctions = nullptr;
+  BytecodeFunction* functions = nullptr;
+};
+
 #endif //QUICKSCRIPT_INTERPRETER_H
