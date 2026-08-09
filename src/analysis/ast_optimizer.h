94e01325a436d99b8eb76d3e9ac90a4a22c76f0e
JulieWoolie <andero.magi.420@gmail.com>
2026-08-09 17:57:55 +0300

    Delete ast_optimizer

--- src/analysis/ast_optimizer.h
+++
@@ -1,8 +0,0 @@
-#ifndef QUICKSCRIPT_AST_OPTIMIZER_H
-#define QUICKSCRIPT_AST_OPTIMIZER_H
-
-#include "../parse/syntaxtree.h"
-
-void optimize(ScriptFileStatement* sfs);
-
-#endif //QUICKSCRIPT_AST_OPTIMIZER_H
