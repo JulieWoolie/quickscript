19784f3647138663ad87016cceb53c3a82538da1
JulieWoolie <andero.magi.420@gmail.com>
2026-08-22 12:04:38 +0300

    Use only one opcode for function invocations

--- deno-scripts/opcode-gen.ts
+++ deno-scripts/opcode-gen.ts
@@ -176,8 +176,7 @@ function functionCallOperations() {
   opCode("LFUNCLOOKUP", [uint32("index"), reg("out")])
   opCode("NFUNCLOOKUP", [uint32("typeindex"), uint64("funcName"), reg("out")])
 
-  opCode("INVOKEV", [reg("func")])
-  byteSizedOpCode("INVOKE", [reg("func")])
+  opCode("INVOKE", [reg("func")])
 }
 
 function conversionOperations() {
