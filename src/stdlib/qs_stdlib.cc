af6feca1cbc04d336fc6075af5bdc5d38218103e
JulieWoolie <andero.magi.420@gmail.com>
2026-08-27 23:53:35 +0300

    Update qs_stdlib.cc

--- src/stdlib/qs_stdlib.cc
+++ src/stdlib/qs_stdlib.cc
@@ -2,18 +2,12 @@
 
 #include "../strings/utf8.h"
 
+#define QS_STR_SPEC "%.*s"
+#define PRINT_QS_STR(str) str.length, reinterpret_cast<conststring>(str.data)
+
 static void qs_println(NativeCall& call) {
   const QsArray str = call.getArrayArgument(0);
-
-  const uint32 len = str.length;
-  const uint32 bufLen = str.length + 1;
-
-  int8 buf[bufLen];
-
-  memcpy(buf, str.data, len);
-  buf[len] = '\0';
-
-  fprintf(stdout, "%s", buf);
+  fprintf(stdout, QS_STR_SPEC, PRINT_QS_STR(str));
 }
 
 void addStandardLibrary(BindingsObject* obj) {
