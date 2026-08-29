eef0f247353c07d3c0bf22b7c10889638483cb88
JulieWoolie <andero.magi.420@gmail.com>
2026-08-29 16:28:50 +0300

    Update qs_stdlib.cc

--- src/stdlib/qs_stdlib.cc
+++ src/stdlib/qs_stdlib.cc
@@ -1,6 +1,7 @@
 #include "qs_stdlib.h"
 
 #include "../strings/utf8.h"
+#include "../types/ConstTypes.h"
 
 #define QS_STR_SPEC "%.*s"
 #define PRINT_QS_STR(str) str.length, reinterpret_cast<conststring>(str.data)
@@ -11,5 +12,9 @@ static void qs_println(NativeCall& call) {
 }
 
 void addStandardLibrary(BindingsObject* obj) {
-  obj->addFunctionBinding("println", "(string)=>void", qs_println);
+  obj->addFunctionBinding(
+    "println",
+    FunctionSignature::make(nullptr, 1, ConstTypes::STRING()),
+    qs_println
+  );
 }
