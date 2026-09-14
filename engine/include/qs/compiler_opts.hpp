5f4fd253985c9281c3db0d12a1005c3835b391f3
JulieWoolie <andero.magi.420@gmail.com>
2026-09-14 17:03:18 +0300

    Add `implicitImports` to compilation options

--- engine/include/qs/compiler_opts.hpp
+++ engine/include/qs/compiler_opts.hpp
@@ -5,6 +5,7 @@ struct CompilationOptions {
   bool exprOptimizing = true;
   bool statOptimizing = true;
   bool includeAsserts = true;
+  std::vector<std::string> implicitImports = {"std"};
 };
 
 #endif //QS_COMPILER_OPTS_H
