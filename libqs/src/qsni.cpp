7dc52134494fc1ad853d466fe3acf0c0b2713c50
JulieWoolie <andero.magi.420@gmail.com>
2026-09-19 21:39:02 +0300

    Update qsni.cpp

--- libqs/src/qsni.cpp
+++ libqs/src/qsni.cpp
@@ -1,21 +1,29 @@
 #include "qsni.h"
 
-#include "qs/compiler_opts.hpp"
 #include "qs/nativeinterface.hpp"
-#include "qs/stringtable.hpp"
-#include "qs/analysis/analyzer.hpp"
-#include "qs/analysis/transformer.hpp"
+#include "qs/objects.hpp"
+#include "qs/qsenv.hpp"
 #include "qs/bytecode/bytecode_file.hpp"
-#include "qs/codegen/compiler.hpp"
 #include "qs/interpreter/interpreter.hpp"
-#include "qs/parse/lexer.hpp"
-#include "qs/parse/parser.hpp"
 #include "qs/types/types.hpp"
 
-struct QsEnvironment {
-  BindingsObject* bindings;
-  CompilationOptions options;
-};
+#define ARRAY_READ_WRITE_FUNCS(shorthand, type) \
+  type qsa_get##shorthand(const QsScriptArray array, const uint32 idx) {\
+    return static_cast<type*>(getArrayDataStart(array))[idx];\
+  }\
+  void qsa_set##shorthand(const QsScriptArray array, const uint32 idx, const type value) {\
+    static_cast<type*>(getArrayDataStart(array))[idx] = value;\
+  }
+
+#define OBJ_READ_WRITE_FUNCS(shorthand, type) \
+  type qsobj_get##shorthand##Property(QsScriptObject obj, const uint64 offset) {\
+    uint8* dataPtr = static_cast<uint8*>(obj) + 1 + offset;\
+    return *reinterpret_cast<type*>(dataPtr);\
+  }\
+  void qsobj_set##shorthand##Property(QsScriptObject obj, const uint64 offset, const type value) {\
+    uint8* dataPtr = static_cast<uint8*>(obj) + 1 + offset;\
+    *reinterpret_cast<type*>(dataPtr) = value;\
+  }
 
 qstypekind qst_getTypeKind(const QsScriptType type) {
   return type->kind();
@@ -29,44 +37,49 @@ conststring qst_getTypeName(const QsScriptType type) {
   return type->getTypeName();
 }
 
-QsScriptType qsarray_getComponentType(const QsScriptType type) {
+QsScriptType qsat_getComponentType(const QsScriptType type) {
   return static_cast<const ScriptArrayType*>(type)->getComponentType();
 }
 
-QsScriptType qstruct_getPropertyType(const QsScriptType type, const uint32 propertyIndex) {
+QsScriptType qsst_getPropertyType(const QsScriptType type, const uint32 propertyIndex) {
   return static_cast<const ScriptStructType*>(type)->getProperty(propertyIndex)->type;
 }
 
-conststring qstruct_getPropertyName(const QsScriptType type, const uint32 propertyIndex) {
+conststring qsst_getPropertyName(const QsScriptType type, const uint32 propertyIndex) {
   return static_cast<const ScriptStructType*>(type)->getProperty(propertyIndex)->name.c_str();
 }
 
-uint64 qstruct_getPropertyOffset(const QsScriptType type, const uint32 propertyIndex) {
+uint64 qsst_getPropertyOffset(const QsScriptType type, const uint32 propertyIndex) {
   return static_cast<const ScriptStructType*>(type)->getProperty(propertyIndex)->offset;
 }
 
-int32 qstruct_getPropertyIndex(const QsScriptType type, const conststring propName) {
-  const ScriptStructType* sType = static_cast<const ScriptStructType*>(type);
-  const uint32 propCount = sType->getPropertyCount();
+int32 qsst_getPropertyIndex(const QsScriptType type, const conststring propName) {
+  const ScriptStructType* structType = static_cast<const ScriptStructType*>(type);
+  const uint32 propCount = structType->getPropertyCount();
 
-  for (uint32 i = 0; i < propCount; i++) {
-    if (sType->getProperty(i)->name == propName) {
-      return i;
-    }
+  if (propCount == 0) {
+    return PROPERTY_NOT_FOUND;
   }
 
-  return -1;
+  for (uint32 i = 0; i < propCount; i++) {
+    if (structType->getProperty(i)->name != propName) {
+      continue;
+    }
+    return i;
+  }
+
+  return PROPERTY_NOT_FOUND;
 }
 
-uint32 qstruct_getPropertyCount(const QsScriptType type) {
+uint32 qsst_getPropertyCount(const QsScriptType type) {
   return static_cast<const ScriptStructType*>(type)->getPropertyCount();
 }
 
-uint64 qstruct_getSize(const QsScriptType type) {
+uint64 qsst_getSize(const QsScriptType type) {
   return static_cast<const ScriptStructType*>(type)->getHeapSize();
 }
 
-uint8 qstruct_getAlignment(const QsScriptType type) {
+uint8 qsst_getAlignment(const QsScriptType type) {
   return static_cast<const ScriptStructType*>(type)->getAlignment();
 }
 
@@ -86,10 +99,63 @@ QsScriptType qfs_getReturnType(const QsScriptType type) {
   return static_cast<const FunctionSignature*>(type)->getReturnType();
 }
 
+uint64 qsa_getLength(const QsScriptArray array) {
+  return readQsArrayLength(array);
+}
+
+uint8* qsa_getData(const QsScriptArray array) {
+  return static_cast<uint8*>(getArrayDataStart(array));
+}
+
+int8* qsa_getCharData(const QsScriptArray array) {
+  return static_cast<int8*>(getArrayDataStart(array));
+}
+
+uint32 qsa_getRefCounter(const QsScriptArray array) {
+  return getArrayRefCounter(array);
+}
+
+void qsa_setRefCounter(const QsScriptArray array, const uint32 refCounter) {
+  setArrayRefCounter(array, refCounter);
+}
+
+ARRAY_READ_WRITE_FUNCS(I8, int8)
+ARRAY_READ_WRITE_FUNCS(U8, uint8)
+ARRAY_READ_WRITE_FUNCS(I16, int16)
+ARRAY_READ_WRITE_FUNCS(U16, uint16)
+ARRAY_READ_WRITE_FUNCS(I32, int32)
+ARRAY_READ_WRITE_FUNCS(U32, uint32)
+ARRAY_READ_WRITE_FUNCS(I64, int64)
+ARRAY_READ_WRITE_FUNCS(U64, uint64)
+ARRAY_READ_WRITE_FUNCS(F32, float32)
+ARRAY_READ_WRITE_FUNCS(F64, float64)
+ARRAY_READ_WRITE_FUNCS(Object, QsScriptObject)
+ARRAY_READ_WRITE_FUNCS(Array, QsScriptArray)
+
+uint32 qsobj_getRefCounter(const QsScriptObject obj) {
+  return getArrayRefCounter(obj);
+}
+
+void qsobj_setRefCounter(const QsScriptObject obj, const uint32 refCounter) {
+  setArrayRefCounter(obj, refCounter);
+}
+
+OBJ_READ_WRITE_FUNCS(I8, int8)
+OBJ_READ_WRITE_FUNCS(U8, uint8)
+OBJ_READ_WRITE_FUNCS(I16, int16)
+OBJ_READ_WRITE_FUNCS(U16, uint16)
+OBJ_READ_WRITE_FUNCS(I32, int32)
+OBJ_READ_WRITE_FUNCS(U32, uint32)
+OBJ_READ_WRITE_FUNCS(I64, int64)
+OBJ_READ_WRITE_FUNCS(U64, uint64)
+OBJ_READ_WRITE_FUNCS(F32, float32)
+OBJ_READ_WRITE_FUNCS(F64, float64)
+OBJ_READ_WRITE_FUNCS(Object, QsScriptObject)
+OBJ_READ_WRITE_FUNCS(Array, QsScriptArray)
+
 uint64 qsc_getReturnValue(const QsNativeCall call) {
   return call->getReturnValue();
 }
-
 boolean qsc_isFailedCall(const QsNativeCall call) {
   return call->isFailedCall();
 }
@@ -114,49 +180,81 @@ void qsc_throwError(const QsNativeCall call, const conststring errorMessage) {
   call->setError(errorMessage);
 }
 
-boolean qsc_getBoolArgument(const QsNativeCall call, const uint32 idx) {
-  return call->getBoolArgument(idx);
-}
-
 QsScriptType qsc_getArgumentType(const QsNativeCall call, const uint32 idx) {
   return call->getArgumentType(idx);
 }
 
-#define LIB_ARG_GETTER(type, shorthand) \
-  type qsc_get##shorthand##Argument(const QsNativeCall call, const uint32 idx) {\
-    return call->get##shorthand##Argument(idx);\
-  }
+boolean qsc_getBoolArgument(const QsNativeCall call, const uint32 idx) {
+  return call->getBoolArgument(idx);
+}
 
-LIB_ARG_GETTER(int8, I8)
-LIB_ARG_GETTER(uint8, U8)
-LIB_ARG_GETTER(int16, I16)
-LIB_ARG_GETTER(uint16, U16)
-LIB_ARG_GETTER(int32, I32)
-LIB_ARG_GETTER(uint32, U32)
-LIB_ARG_GETTER(int64, I64)
-LIB_ARG_GETTER(uint64, U64)
-LIB_ARG_GETTER(float32, F32)
-LIB_ARG_GETTER(float64, F64)
+uint8 qsc_getU8Argument(const QsNativeCall call, const uint32 idx) {
+  return call->getU8Argument(idx);
+}
+
+int8 qsc_getI8Argument(const QsNativeCall call, const uint32 idx) {
+  return call->getI8Argument(idx);
+}
+
+uint16 qsc_getU16Argument(const QsNativeCall call, const uint32 idx) {
+  return call->getU16Argument(idx);
+}
+
+int16 qsc_getI16Argument(const QsNativeCall call, const uint32 idx) {
+  return call->getI16Argument(idx);
+}
+
+uint32 qsc_getU32Argument(const QsNativeCall call, const uint32 idx) {
+  return call->getU32Argument(idx);
+}
+
+int32 qsc_getI32Argument(const QsNativeCall call, const uint32 idx) {
+  return call->getI32Argument(idx);
+}
+
+uint64 qsc_getU64Argument(const QsNativeCall call, const uint32 idx) {
+  return call->getU64Argument(idx);
+}
+
+int64 qsc_getI64Argument(const QsNativeCall call, const uint32 idx) {
+  return call->getI64Argument(idx);
+}
+
+float32 qsc_getF32Argument(const QsNativeCall call, const uint32 idx) {
+  return call->getF32Argument(idx);
+}
+
+float64 qsc_getF64Argument(const QsNativeCall call, const uint32 idx) {
+  return call->getF64Argument(idx);
+}
+
+QsScriptArray qsc_getArrayArgument(const QsNativeCall call, const uint32 idx) {
+  return reinterpret_cast<QsScriptArray>(call->getU64Argument(idx));
+}
+
+QsScriptObject qsc_getObjectArgument(QsNativeCall call, uint32 idx) {
+  return reinterpret_cast<QsScriptObject>(call->getU64Argument(idx));
+}
 
 uint32 qsbf_loadBytecodeFile(const void* buf, const uint64 bufferSize, QsBytecodeFile* fileOut) {
   BytecodeFile& bf = BytecodeFile::create();
-  BytecodeReadResult res = deserializeBytecodeFile(static_cast<const uint8*>(buf), bufferSize, bf);
+  const BytecodeReadResult result = deserializeBytecodeFile(static_cast<const uint8*>(buf), bufferSize, bf);
 
-  if (res != IR_RESULT_OK) {
+  if (result != IR_RESULT_OK) {
     BytecodeFile::destroy(bf);
-    return res;
+  } else {
+    *fileOut = &bf;
   }
 
-  *fileOut = &bf;
-  return IR_RESULT_OK;
+  return result;
 }
 
 conststring qsbf_getResultString(const uint32 result) {
   return getReadResultMessage(result);
 }
 
-void qsbf_freeBytecodeFile(const QsBytecodeFile bFile) {
-  BytecodeFile::destroy(*static_cast<BytecodeFile*>(bFile));
+void qsbf_freeBytecodeFile(QsBytecodeFile bFile) {
+  BytecodeFile::destroy(*bFile);
 }
 
 QsEnv qse_createEnv() {
@@ -164,59 +262,66 @@ QsEnv qse_createEnv() {
 }
 
 void qse_freeEnv(const QsEnv env) {
-  delete static_cast<QsEnvironment*>(env);
+  delete env;
 }
 
-void qse_addStandardLibraries(const QsEnv env) {
-  // idk atm
+void qse_addStandardLibraries(QsEnv env) {
+  // idk man
 }
 
 void qse_addLibraryDirectory(const QsEnv env, const conststring dirPath) {
-  // idk atm
+  env->getLibraryDirectories().push_back(dirPath);
 }
 
 void qse_registerNative(
-  QsEnv env,
-  conststring funcName,
-  conststring signature,
-  QsNativeFunction func
+  const QsEnv env,
+  const conststring ns,
+  const conststring funcName,
+  const conststring signature,
+  const QsNativeFunction func
 ) {
-  QsEnvironment* e = static_cast<QsEnvironment*>(env);
+  env->registerNative(ns, funcName, signature, func);
+}
 
+void qse_registerNatives(const QsEnv env, const conststring ns, const uint32 n, ...) {
+  va_list l;
+  va_start(l, n);
+
+  for (uint32 i = 0; i < n; i++) {
+    const conststring fName = va_arg(l, conststring);
+    const conststring sig = va_arg(l, conststring);
+    const QsNativeFunction func = va_arg(l, QsNativeFunction);
+    env->registerNative(ns, fName, sig, func);
+  }
+
+  va_end(l);
 }
 
 void qse_setStatementInlining(const QsEnv env, const boolean state) {
-  static_cast<QsEnvironment*>(env)->options.statOptimizing = state;
+  env->getOptions().statOptimizing = state;
 }
 
 void qse_setExpressionInlining(const QsEnv env, const boolean state) {
-  static_cast<QsEnvironment*>(env)->options.exprOptimizing = state;
+  env->getOptions().exprOptimizing = state;
 }
 
 void qse_setAssertsCompiled(const QsEnv env, const boolean state) {
-  static_cast<QsEnvironment*>(env)->options.includeAsserts = state;
+  env->getOptions().includeAsserts = state;
 }
 
-QsVirtualMachine qsvm_createVirtualMachine(const QsEnv env) {
-  VirtualMachine* vm = new VirtualMachine();
-  const QsEnvironment* e = static_cast<QsEnvironment*>(env);
-
-  if (e->bindings) {
-    vm->addBindings(e->bindings);
-  }
-
-  return vm;
+QsVirtualMachine qsvm_createVirtualMachine(QsEnv env) {
+  return new VirtualMachine();
 }
 
 void qsvm_freeVirtualMachine(const QsVirtualMachine vm) {
-  delete static_cast<VirtualMachine*>(vm);
+  delete vm;
 }
 
-void qsvm_loadBytecodeFile(const QsVirtualMachine vm, const QsBytecodeFile bFile, const conststring name) {
-  static_cast<VirtualMachine*>(vm)->addBytecodeFile(*static_cast<const BytecodeFile*>(bFile), name);
+void qsvm_loadBytecodeFile(QsVirtualMachine vm, QsBytecodeFile bFile, conststring name) {
+
 }
 
-int32 qsvm_beginExecution(QsVirtualMachine vm) {
+int32 qsvm_beginExecution(QsVirtualMachine vm, uint32 argc, cstring* argv) {
 
 }
 
@@ -225,84 +330,48 @@ uint64 qsvm_callFunction(QsVirtualMachine vm, QsFunction sf) {
 }
 
 conststring qsvm_toString(const QsVirtualMachine vm, const uint64 qsValue, const QsScriptType type) {
-  VirtualMachine* qsvm = static_cast<VirtualMachine*>(vm);
-  const typeindex typeIdx = qsvm->getTypes().findIndex(static_cast<const ScriptType*>(type));
-  std::string result = "";
-  qsvm->toString(result, typeIdx, qsValue);
+  std::string result;
+  const typeindex idx = vm->getTypes().findIndex(type);
 
-  const uint32 strSize = result.length();
+  vm->toString(result, idx, qsValue);
 
-  int8* buf = static_cast<int8*>(malloc(strSize + 1));
-  memcpy(buf, result.data(), strSize);
-  buf[strSize] = 0;
+  if (result.empty()) {
+    return "";
+  }
+
+  int8* buf = static_cast<int8*>(malloc(result.length() + 1));
+  memcpy(buf, result.data(), result.length());
+  buf[result.length()] = '\0';
 
   return buf;
 }
 
-boolean qsvm_equals(const QsVirtualMachine vm, const uint64 a, const uint64 b, const QsScriptType type) {
-  const typeindex typeIdx = vm->getTypes().findIndex(type);
-  return vm->equals(a, b, typeIdx);
+boolean qsvm_equals(QsVirtualMachine vm, uint64 a, uint64 b, QsScriptType type) {
+  const typeindex idx = vm->getTypes().findIndex(type);
+  return vm->equals(a, b, idx);
 }
 
-boolean qsc_compileSourceFile(conststring sourceName, conststring source, QsEnv env, QsBytecodeFile* fileOut) {
-  QsEnvironment* e = static_cast<QsEnvironment*>(env);
+QsScriptObject qsvm_makeStruct(QsVirtualMachine vm, QsScriptType structType) {
 
-  std::string fileContent = source;
+}
 
-  CompilerErrors errors = CompilerErrors(&fileContent, sourceName);
-  StringTable table = StringTable();
-  TokenList tokens = TokenList();
+void qsvm_freeStruct(const QsVirtualMachine vm, const QsScriptObject obj) {
+  vm->getHeap().freeMemory(obj);
+}
 
-  Lexer l = Lexer(fileContent, &tokens, &table, &errors);
+QsScriptArray qsvm_makeArray(QsVirtualMachine vm, QsScriptType arrayType, uint32 capacity) {
 
-  try {
-    l.next();
-    l.lex();
-  } catch (std::runtime_error& error) {
-    return false;
-  }
+}
 
-  NoFreeAllocator alloc = NoFreeAllocator();
+void qsvm_freeArray(const QsVirtualMachine vm, const QsScriptArray array) {
+  vm->getHeap().freeMemory(array);
+}
 
-  Parser p = Parser(&tokens, &alloc, &errors, &table);
-  ScriptFileStatement* sfs = nullptr;
-
-  try {
-    sfs = p.parse();
-  } catch (std::runtime_error& error) {
-    return false;
-  }
-
-  TypeTable types = TypeTable();
-
-  BindingsObject* bindings = nullptr;
-  bool tempBindings = false;
-
-  if (e->bindings) {
-    bindings = e->bindings;
-  } else {
-    bindings = BindingsObject::create();
-    tempBindings = true;
-  }
-
-  SemanticContext ctx = SemanticContext(types, table, errors, alloc, e->options, bindings);
-  runSemanticAnalysis(sfs, ctx);
-
-  if (errors.getErrorCount() != 0) {
-    if (tempBindings) {
-      BindingsObject::destroy(bindings);
-    }
-    return false;
-  }
-
-  runSemanticTransformer(ctx, sfs);
-
-  BytecodeFile& bFile = compile(ctx);
-  *fileOut = &bFile;
-
-  if (tempBindings) {
-    BindingsObject::destroy(bindings);
-  }
-
-  return true;
+boolean qsc_compileSourceFile(
+  conststring sourceName,
+  conststring source,
+  QsEnv env,
+  QsBytecodeFile* fileOut
+) {
+  return env->compileSourceFile(source, sourceName, fileOut);
 }
