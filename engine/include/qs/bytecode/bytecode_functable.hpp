af2aaf3d6b21cfcc3d8d5083360998a58e5aec35
JulieWoolie <andero.magi.420@gmail.com>
2026-09-19 21:41:01 +0300

    Add function flags for bytecode function table entries

--- engine/include/qs/bytecode/bytecode_functable.hpp
+++ engine/include/qs/bytecode/bytecode_functable.hpp
@@ -4,11 +4,15 @@
 #include "qs/common.hpp"
 #include "qs/types/TypeTable.hpp"
 
+#define FUNCFLAG_NATIVE 0x1
+typedef uint16 functionflags;
+
 struct FunctionTableEntry {
   uint64 nameOffset = 0;
   typeindex signatureIndex = 0;
   uint64 startingInstruction = 0;
   uint64 stackSize = 0;
+  functionflags flags = 0;
 };
 
 #endif //QS_BYTECODE_FUNCTABLE_H
